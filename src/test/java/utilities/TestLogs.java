6985f1dfe40164ccbb6e70c035b0df3df34c7b0b
saimCybertek <saim@cybertekschool.com>
2020-03-08 17:16:43 -0500

    Surefire plugin setup

--- src/test/java/utilities/TestLogs.java
+++ src/test/java/utilities/TestLogs.java
@@ -1,24 +1,11 @@
 package utilities;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-
 public class TestLogs {
 
     public static void main(String[] args) {
 
-        ExtentHtmlReporter configs = new ExtentHtmlReporter("./extentReport/report.html");
-        configs.config().setTheme(Theme.DARK);
-        ExtentReports reports = new ExtentReports();
-        reports.attachReporter(configs);
-        System.out.println("start test");
-        ExtentTest extentTest = reports.createTest("Google Search");
-        extentTest.pass("It is passing");
-        extentTest.fail("didn't match. FAIL!");
-        reports.flush();
-        System.out.println("completed");
+        Driver.getDriver().get("http://www.google.com");
+
     }
 
 }
